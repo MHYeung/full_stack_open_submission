850f3d960833646940f440666210d1ba1ad451f8
MHYeung <johnyeung1998@gmail.com>
2023-03-12 10:20:48 +0800

    ex1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,18 +4,36 @@ const Statistics = (props) => {
   return props.all !== 0 ? (
     <div>
       <h3>Statistics</h3>
-      <p>Good: {props.good}</p>
+      <StatisticsLine text = "Good" value = {props.good}/>
+      <StatisticsLine text = "Neutral" value = {props.neutral}/>
+      <StatisticsLine text = "Bad" value = {props.bad}/>
+      <StatisticsLine text = "All" value = {props.all}/>
+      <StatisticsLine text = "Average" value = {(props.good+props.bad*-1)/props.all}/>
+      <StatisticsLine text = "Positive" value =  {(props.good/props.all*100) + "%"}/>
+      {/* <p>Good: {props.good}</p>
       <p>Neutral: {props.neutral}</p>
       <p>Bad: {props.bad}</p>
       <p>All: {props.all}</p>
       <p>Average: {(props.good + props.bad * -1) / props.all}</p>
-      <p>Positive: {(props.good / props.all) * 100}%</p>
+      <p>Positive: {(props.good / props.all) * 100}%</p> */}
     </div>
   ) : (
     <div>
       <h3>Statistics</h3>
       <p>No feedback given</p>
     </div>
+  );
+};
+
+const StatisticsLine = (props) => {
+  return(
+    <p><strong>{props.text}:</strong> {props.value}</p>
+  )
+}
+
+const Button = (props) => {
+  return (
+      <button onClick={props.onClick}>{props.description}</button>
   );
 };
 
@@ -28,10 +46,10 @@ const App = () => {
   return (
     <div>
       <h3>Give feedback</h3>
-      <button onClick={() => setGood(good + 1)}> Good </button>
-      <button onClick={() => setNeutral(neutral + 1)}> Neutral </button>
-      <button onClick={() => setBad(bad + 1)}> Bad </button>
-      
+      <Button onClick={() => setGood(good + 1)} description="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} description="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} description="Bad" />
+
       <Statistics
         good={good}
         neutral={neutral}
